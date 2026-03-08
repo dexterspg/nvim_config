86ac85dde05ce7ca8667bfa20f7fc0c83a84c49d
Dexter Pagkaliwangan <dexterspg@gmail.com>
2026-03-07 21:39:01 -0500

    Add .projectroot pattern for project root detection in parent directories

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -7,7 +7,7 @@ return {
             on_config_done = nil,
             manual_mode = false,
             detection_methods = { "pattern" },
-            patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml" },
+            patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml", ".projectroot" },
             ignore_lsp = {},
             exclude_dirs = {},
             show_hidden = false,
